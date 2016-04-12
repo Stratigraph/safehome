cca023e5556807dd94d00778d10f5461cf7e9991
Chris Daley <chebizarro@gmail.com>
2016-04-11 21:39:10 -0700

    added static files

--- safehome/settings.py
+++ safehome/settings.py
@@ -148,6 +148,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
     os.path.join(STATIC_ROOT, "bootstrap"),
     os.path.join(STATIC_ROOT, "startbootstrap-sb-admin-2"),
     os.path.join(STATIC_ROOT, "jquery"),
