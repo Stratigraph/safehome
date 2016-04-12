77227010c83fcd4e7106ea2f2d2f1be0f4493440
Chris Daley <chebizarro@gmail.com>
2016-04-11 21:31:56 -0700

    added STATIC_ROOT

--- safehome/settings.py
+++ safehome/settings.py
@@ -147,9 +147,9 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, "static"),
+#]
 
 
 ACCOUNT_USERNAME_REQUIRED = False
